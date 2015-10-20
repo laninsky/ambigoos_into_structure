a429ddbae9ec55f72c5cc66023e337b983827232
Alana Alexander <laninsky@gmail.com>
2015-10-20 09:35:34 -0500

    Create ambigoos.R

---
+++ ambigoos.R
@@ -0,0 +1,8 @@
+library(stringr)
+
+temp <- read.table("temp",header=FALSE,stringsAsFactors=FALSE,sep="\t")
+samplenames <- read.table("samplenames",header=FALSE,stringsAsFactors=FALSE,sep="\t")
+
+
+rows <- dim(temp1)[1]
+
