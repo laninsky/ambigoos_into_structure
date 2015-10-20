fff804d83037c443dd50f08af58804276393bcc9
Alana Alexander <laninsky@gmail.com>
2015-10-20 15:25:32 -0500

    Update ambigoos.R

--- ambigoos.R
+++ ambigoos.R
@@ -3,6 +3,5 @@ library(stringr)
 temp <- read.table("temp",header=FALSE,stringsAsFactors=FALSE,sep="\t")
 samplenames <- read.table("samplenames",header=FALSE,stringsAsFactors=FALSE,sep="\t")
 
-
 rows <- dim(temp1)[1]
 
