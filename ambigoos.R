1b9ebc681793a51c9187fcf2b4ec196510244ff2
Alana Alexander <laninsky@gmail.com>
2015-10-23 20:37:37 -0500

    Update ambigoos.R

--- ambigoos.R
+++ ambigoos.R
@@ -14,6 +14,24 @@ if ((length(grep("fa*",temp[i,1])))>0) {
 ambigootemp <- paste(temp[i,1], j)
 ambigoo_key <- rbind(ambigoo_key,ambigootemp)
 j <- j + 1
+seqlength <- nchar(temp[(i+2),1])
+seqpos <- seq(2,(2*no_taxa),2)
+
+tempstructure <- matrix(NA,nrow=no_taxa,ncol=seqlength) 
+
+for (k in 1:(length(seqpos))) {
+tempstructure[k,] <- unlist(strsplit(temp[(seqpos[k]+1),1],""))
+}
 
 
 
+
+tempstructure[1:no_taxa,1] <- samplenames[1:no_taxa,1]
+
+
+
+i <- i + 2*(no_taxa) + 1
+} else {
+i <- i + 1
+}
+}
