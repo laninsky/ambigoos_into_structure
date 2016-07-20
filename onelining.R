87913ecb99b7645ce0e3c6dcbe6ebe1f36405cc5
Alana Alexander <laninsky@gmail.com>
2016-07-20 10:50:17 -0500

    Update onelining.R

--- onelining.R
+++ onelining.R
@@ -18,6 +18,13 @@ sequencepaste <- paste(sequencepaste,intable[j,1],sep="")
 
 to_write <- rbind(to_write,toupper(sequencepaste))
 
+forsort <- matrix(NA,ncol=2,nrow=((dim(to_write)[1])/2))
+forsort[,1] <- to_write[(seq(1,(dim(to_write)[1]),2)),1]
+forsort[,2] <- to_write[(seq(2,(dim(to_write)[1]),2)),1]
+forsort <- forsort[order(forsort[,1]),]
+to_write[(seq(1,(dim(to_write)[1]),2)),1] <- forsort[,1]
+to_write[(seq(2,(dim(to_write)[1]),2)),1] <- forsort[,2]
+
 write.table(to_write, "tempout",quote=FALSE, col.names=FALSE,row.names=FALSE)
 
 q()
